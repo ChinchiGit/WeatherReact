779de49b6de588c1a5219a92b78a574aedbc35ba
ChinchiGit <carloschinchillamoreno@gmail.com>
2024-07-05 09:54:18 +0200

    Installed necessary dependencies and set up configuration files for testing functionality

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+      '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
+    },
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest'
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  };
+  
