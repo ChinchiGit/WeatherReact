01c77ec16b76d9e55e0a8a372d1f57fa71c7ccbe
ChinchiGit <carloschinchillamoreno@gmail.com>
2024-07-08 20:39:20 +0200

    chore: Add unit tests for FormCity, ErrorMessage, and WeatherCard components

--- src/components/Main/WeatherList/ErrorMessage/ErrorMessage.test.js
+++ src/components/Main/WeatherList/ErrorMessage/ErrorMessage.test.js
@@ -1,10 +1,17 @@
 import React from "react";
-
+import { render, screen } from '@testing-library/react';
 import ErrorMessage from "./ErrorMessage";
 
 describe("ErrorMessage", () => {
-  test("matches snapshot", () => {
-   
+  test("renders error message correctly", () => {
+    render(<ErrorMessage />);
 
+    const errorImage = screen.getByAltText("Error en la búsqueda");
+    const errorMessageTitle = screen.getByText("Error. No hay resultados para tu búsqueda.");
+    const errorMessageText = screen.getByText("Introduce de nuevo una ciudad.");
+
+    expect(errorImage).toBeInTheDocument();
+    expect(errorMessageTitle).toBeInTheDocument();
+    expect(errorMessageText).toBeInTheDocument();
   });
 });
